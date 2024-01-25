d7be22b20bec7e00617676d80c3e2d16190b3588
AdrakPro <adrak99.adrak99@gmail.com>
2024-01-25 19:02:11 +0100

    Style formatting

--- statistics/ctg_sim/main.py
+++ statistics/ctg_sim/main.py
@@ -8,19 +8,27 @@ from random_generator import generate_sequences
 # zmiennych losowych o tym samym rozkładzie (niekoniecznie normalnym) zbiega do
 # rozkładu normalnego (przy n dążącym do nieskończoności).
 
-def plot_histogram(data, n, color='blue'):
+
+def plot_histogram(data, n):
     plt.figure()
-    plt.hist(data, bins=30, density=True, color=color, edgecolor='black')
+    plt.hist(data, bins=30, density=True, color="blue", edgecolor="black")
 
     # Average and standard deviation of data
     mean_data, std_data = np.mean(data), np.std(data)
-    normal_x = np.linspace(min(data), max(data), 100)
-    normal_distribution = ((1 / (std_data * np.sqrt(2 * np.pi))) *
-                           np.exp(-(normal_x - mean_data) ** 2 / (2 * std_data ** 2)))
 
-    plt.plot(normal_x, normal_distribution, color='red', linewidth=2,
-             label='Normal Distribution')
-    plt.title(f'Histogram for n={n}')
+    normal_x = np.linspace(min(data), max(data), 100)
+    normal_distribution = (1 / (std_data * np.sqrt(2 * np.pi))) * np.exp(
+        -((normal_x - mean_data) ** 2) / (2 * std_data**2)
+    )
+
+    plt.plot(
+        normal_x,
+        normal_distribution,
+        color="red",
+        linewidth=2,
+        label="Normal Distribution",
+    )
+    plt.title(f"Histogram for n={n}")
     plt.legend()
     plt.show()
 
@@ -29,27 +37,30 @@ def f(x):
     return (x + 1) * (-1 < x < 0) + (-x + 1) * (0 <= x < 1)
 
 
-if __name__ == '__main__':
-    y = generate_sequences(distribution=f,
-                           start=-1,
-                           stop=1,
-                           upper_bound=1,
-                           number_of_sample=100_000)
-
+if __name__ == "__main__":
     total_samples = 100_000
     sub_sequences = 10_000
-    n_values = [1, 2, 5, 10]
+    sub_sequences_length = [1, 2, 5, 10]
 
-    for n in n_values:
+    y = generate_sequences(
+        distribution=f, start=-1, stop=1, upper_bound=1, number_of_sample=total_samples
+    )
+
+    for n in sub_sequences_length:
         # Generate a sequence of random variables
-        y_sequence = generate_sequences(distribution=f, start=-1, stop=1, upper_bound=1,
-                                        number_of_sample=total_samples)
+        y_sequence = generate_sequences(
+            distribution=f,
+            start=-1,
+            stop=1,
+            upper_bound=1,
+            number_of_sample=total_samples,
+        )
 
         # Divide the sequence into m equal subsequences of length n. Example:
-        # data = [1,2,3,4,5,6] the subsequence for n=2 is [data[1],data[1+2],data[3+2],...],
-        y_subsequences = y_sequence[:sub_sequences * n].reshape(sub_sequences, n)
+        # data=[1,2,3,4,5,6] the subsequence for n=2 is [data[1],data[1+2],data[3+2],...]
+        y_subsequences = y_sequence[: sub_sequences * n].reshape(sub_sequences, n)
 
         # Calculate the average for each subsequence
         y_averages = np.mean(y_subsequences, axis=1)
 
-        plot_histogram(y_averages, n, color='blue')
+        plot_histogram(y_averages, n)
--- statistics/ctg_sim/random_generator.py
+++ statistics/ctg_sim/random_generator.py
@@ -3,20 +3,25 @@ import numpy as np
 from uniform import rand_one_number
 
 
-def generate_sequences(distribution: callable,
-                       start: float,
-                       stop: float,
-                       upper_bound: float,
-                       number_of_sample):
+def generate_sequences(
+    distribution: callable,
+    start: float,
+    stop: float,
+    upper_bound: float,
+    number_of_sample,
+):
     output = list()
     x = np.pi - np.floor(np.pi)
     y = np.e - np.floor(np.e)
     i = 0
+
     while len(output) <= number_of_sample:
         i += 1
         x = rand_one_number(seed=x, saw_teeth=7)
         y = rand_one_number(seed=y, saw_teeth=7)
         if distribution(x * (stop - start) + start) > upper_bound * y:
             output.append(x * (stop - start) + start)
-    print(f'Accept level: {number_of_sample / i * 100:.2f} %')
+
+    print(f"Accept level: {number_of_sample / i * 100:.2f} %")
+
     return np.array(output)
